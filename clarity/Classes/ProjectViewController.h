3b9e3d7d94b5524259d77bab9f2b59cee933ddd3
Erin Parker <erinchanparker@gmail.com>
2013-11-18 19:43:36 -0800

    Created basic PlanViewController scaffold

---
+++ clarity/Classes/ProjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectViewController.h
+//  clarity
+//
+//  Created by Erin Parker on 11/18/13.
+//  Copyright (c) 2013 Spitfire Athlete - Hackathons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectViewController : UITableViewController
+
+@end
