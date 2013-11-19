93c4b76a0eccac00d159c00612b033efa8d367b5
Erin Parker <erinchanparker@gmail.com>
2013-11-19 01:06:46 -0800

    Started creating DashboardView

---
+++ clarity/Classes/DashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController.h
+//  
+//
+//  Created by Erin Parker on 11/19/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardViewController : UITableViewController
+
+@end
